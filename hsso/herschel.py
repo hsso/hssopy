ec8a7e401c6c13d95c9ccc0a59214537384f8422
Miguel de Val-Borro <miguel.deval@gmail.com>
2011-08-21 18:53:28 +0200

    Add HifiMap class

--- hsso/herschel.py
+++ hsso/herschel.py
@@ -29,7 +29,47 @@ def size(arcsec, delta=1): # km
     delta: distance to Earth (AU)"""
     return delta*ac.AU*math.sin(arcsec/3600.*math.pi/180)/1e5
 
-def hifimap(filename, freq0, sideband='USB', subband=1, obsid=1):
+class HifiMap(object):
+    """Calculate line intensity map and coordinates"""
+
+    def __init__(self, filename, freq0, sideband='USB', subband=1, correct=True):
+        self.filename = filename
+        # read HSA FITS file
+        hdulist = pyfits.open(filename)
+        self.ntables = len(hdulist)
+        self.npoints = hdulist[1].data.field('flux_1').shape[0]
+        self.longitudes = np.zeros((self.ntables-1, self.npoints))
+        self.latitudes = np.zeros((self.ntables-1, self.npoints))
+        self.fvals = np.zeros((self.ntables-1, self.npoints))
+        for j in range(1, self.ntables):
+            for k in range(self.npoints):
+                # mid-date observing time in UT
+                integration_time = hdulist[j].data.field('integration time')[k]
+                # if the integration time is not a scalar
+                if not isinstance(integration_time, float):
+                    integration_time = integration_time[subband-1]
+                timestr = datetime(year=1958,month=1,day=1,hour=0,minute=0,second=0) + \
+                        timedelta(microseconds=hdulist[j].data.field('obs time')[k] + \
+                        integration_time/2.)
+                # interpolate and subtract ra and dec of the comet
+                self.longitudes[j-1,k] = hdulist[j].data.field('longitude')[k] 
+                self.latitudes[j-1,k] = hdulist[j].data.field('latitude')[k] 
+                if correct:
+                    self.longitudes[j-1,k] -=  gildas.deltadot(timestr,
+                        filename="/home/miguel/HssO/Hartley2/python/horizons.txt", column=2)
+                    self.latitudes[j-1,k] -= gildas.deltadot(timestr,
+                        filename="/home/miguel/HssO/Hartley2/python/horizons.txt", column=3)
+                # read frequency and flux
+                freq = hdulist[j].data.field('{0}frequency_{1}'.format(sideband.lower(), subband))[k]
+                flux = hdulist[j].data.field('flux_{0}'.format(subband))[k]
+                vel = gildas.vel(freq, freq0)
+                # subtract baseline
+                basep = gildas.basepoly(vel, flux, [1.4, 5], deg=1)
+                flux -= basep(vel)
+                # define line intensity map
+                self.fvals[j-1, k] = gildas.intens(flux, vel, [-.5, .5])[0]
+
+def hifimap(filename, freq0, sideband='USB', subband=1, obsid=1, correct=True):
     """Calculate intensity and coordinates"""
     # read HSA FITS file
     hdulist = pyfits.open(filename)
@@ -50,9 +90,12 @@ def hifimap(filename, freq0, sideband='USB', subband=1, obsid=1):
                     timedelta(microseconds=hdulist[j].data.field('obs time')[k] + \
                     integration_time/2.)
             # interpolate and subtract ra and dec of the comet
-            longitudes[j-1,k] = hdulist[j].data.field('longitude')[k] -  gildas.deltadot(timestr,
+            longitudes[j-1,k] = hdulist[j].data.field('longitude')[k] 
+            latitudes[j-1,k] = hdulist[j].data.field('latitude')[k] 
+            if correct:
+                longitudes[j-1,k] -=  gildas.deltadot(timestr,
                     filename="/home/miguel/HssO/Hartley2/python/horizons.txt", column=2)
-            latitudes[j-1,k] = hdulist[j].data.field('latitude')[k] - gildas.deltadot(timestr,
+                latitudes[j-1,k] -= gildas.deltadot(timestr,
                     filename="/home/miguel/HssO/Hartley2/python/horizons.txt", column=3)
             # read frequency and flux
             freq = hdulist[j].data.field('{0}frequency_{1}'.format(sideband.lower(), subband))[k]
