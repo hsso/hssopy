66a6a9fe79125fa569e58e24a7a6aae450167b9a
Miguel de Val-Borro <miguel.deval@gmail.com>
2014-05-05 18:59:42 -0400

    Add PACS map

--- herschel.py
+++ herschel.py
@@ -249,3 +249,135 @@ def writeto_fits(filename, columns):
         cols.append(pyfits.Column(name=i, format='E', array=j))
     tbhdu = pyfits.new_table(pyfits.ColDefs(cols))
     tbhdu.writeto(filename)
+
+class Pacsmap(object):
+    """Read PACS photometry map"""
+
+    def __init__(self, obsid, size=60, zoom=0, comet=True):
+        """return patch centered on the nucleus"""
+        if isinstance(obsid, int):
+            self.fitsfile = glob.glob(join(datadir, str(obsid), 'level2',
+                'HPPPMAP{}'.format(args.band[0].upper()), '*fits.gz'))[0]
+        else:
+            self.fitsfile = obsid
+        self.hdus = pyfits.open(self.fitsfile)
+        self.cdelt2 = self.hdus[1].header['CDELT2']*3600
+
+        # swap to little-endian byte order to avoid matplotlib bug
+        pmap = self.hdus[1].data
+        if comet:
+            # calculate comet position at midtime
+            date_obs = self.hdus[0].header['DATE-OBS']
+            date_end = self.hdus[0].header['DATE-END']
+            start = datetime.strptime(date_obs, "%Y-%m-%dT%H:%M:%S.%f")
+            end = datetime.strptime(date_end, "%Y-%m-%dT%H:%M:%S.%f")
+            mid_time = start + (end-start)/2
+            # interpolate ra and dec of the comet
+            ra = gildas.deltadot(mid_time, filename=horizons_file[args.obsid],
+                    column=2)
+            dec = gildas.deltadot(mid_time, filename=horizons_file[args.obsid],
+                    column=3)
+            # calculate direction toward the Sun
+            phase_ang = gildas.deltadot(mid_time,
+                    filename=horizons_file[args.obsid], column=8)
+            alpha = 3*np.pi/2 - phase_ang*np.pi/180
+            cos, sin = np.cos(alpha), np.sin(alpha)
+            # origin coordinate is 0 (Numpy and C standards)
+            wcs = pywcs.WCS(self.hdus[1].header)
+            comet = wcs.wcs_sky2pix([(ra, dec)], 0)[0]
+            com = [int(round(i)) for i in comet]
+            sh  = comet-com
+            # shift array to center on comet nucleus
+            pmap = ndimage.interpolation.shift(pmap, sh[::-1])
+            self.pix = np.abs(self.cdelt2)
+            self.fov = int(round(size/self.pix))
+            # patch with 2fovx2fov
+            self.patch = pmap[com[1]-self.fov:com[1]+self.fov+1,
+                              com[0]-self.fov:com[0]+self.fov+1]
+        else:
+            self.patch = pmap
+        if zoom: self.patch = ndimage.zoom(self.patch, zoom, order=2)
+        if args.debug:
+            plt.imshow(pmap, origin="lower")
+#             plt.scatter(*comet)
+            plt.show()
+            plt.close()
+
+    def com(self, size=30):
+        """select the top 0.99% pixels to calculate the center of mass"""
+        fov = int(round(size/self.pix))
+        center = self.patch.shape[0]/2
+        zoom = self.patch[center-fov:center+fov+1,
+                            center-fov:center+fov+1]
+        hist, bins = np.histogram(zoom.ravel(), normed=True, bins=100)
+        threshold = bins[np.cumsum(hist) * (bins[1] - bins[0]) > 0.992][0]
+        mpatch = np.ma.masked_less(zoom, threshold)
+        mapcom = ndimage.measurements.center_of_mass(mpatch)
+        if args.debug:
+            plt.imshow(self.patch, origin="lower")
+            mapmax = ndimage.measurements.maximum_position(zoom)[::-1]
+            plt.scatter(*mapmax)
+            # plot center-of-mass
+            plt.scatter(*mapcom[::-1], color='r')
+            plt.show()
+            plt.close()
+        # center of mass
+        self.com = [int(round(i)) for i in mapcom]
+        # fraction of pixel to com
+        self.sh  = np.array(mapcom) - self.com
+        self.com = [center - fov + i for i in mapcom]
+
+    def shift(self, center, size=30):
+        """shift array to be centerd at cneter"""
+        self.comet = [self.fov-center[0], self.fov-center[1]]
+        self.fov = int(round(size/self.pix))
+        self.patch = self.patch[center[0]-self.fov:center[0]+self.fov+1,
+                                center[1]-self.fov:center[1]+self.fov+1]
+
+    def gauss_fit(self):
+        """fit 2D Gaussian"""
+        pass
+
+    def add(self, pmap):
+        """average orthogonal scans"""
+        self.patch = np.average((self.patch, pmap.patch), axis=0)
+
+    def radprof(self, center=None, binsize=0, rmax=0):
+        """calculate radial profile"""
+        y, x = np.indices(self.patch.shape)
+        if center:
+            i, j = center
+        else:
+            j, i = np.unravel_index(np.argmax(self.patch), self.patch.shape)
+        r = np.sqrt((x-i)**2 + (y-j)**2)
+        ind = np.argsort(r.flat)
+        r *= self.cdelt2
+        sr = r.flat[ind]
+        sim = self.patch.flat[ind]
+        sr = sr[sim >0]
+        sim = sim[sim >0]
+        # normalize to Jy arcsec-2
+        sim /= self.cdelt2**2
+        if binsize:
+            sr /= binsize
+            ri = sr.astype(np.int16)
+            deltar = ri[1:] - ri[:-1]
+            rind = np.where(deltar)[0]
+            rind = np.concatenate(([0], rind+1, [len(ri)]))
+            n = rind[1:] - rind[:-1]
+            self.rprof = np.array([np.mean(sim[lo:hi]) for lo,hi in
+                                    zip(rind[:-1], rind[1:])])
+            self.rprof_e = np.array([np.std(sim[lo:hi]) for lo,hi in
+                                    zip(rind[:-1], rind[1:])])
+            self.rprof_e = np.where(self.rprof > self.rprof_e, self.rprof_e,
+                                    0.99*self.rprof)
+            self.r = binsize*(np.unique(ri)[:-1] + 0.5)
+        else:
+            self.r = sr
+            self.rprof = sim
+            self.rprof_e = np.zeros(len(sr))
+        if rmax:
+            mask = [self.r < rmax]
+            self.r = self.r[mask]
+            self.rprof = self.rprof[mask]
+            self.rprof_e = self.rprof_e[mask]
