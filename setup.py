9967bd442c71fe34ca139de2d90857081bcb0165
Miguel de Val-Borro <miguel.deval@gmail.com>
2014-08-19 15:42:55 -0400

    Use find_packages function

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python
 
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(name='hsso',
     description='Herschel Data Analysis Utilities',
     author='Miguel de Val-Borro',
     author_email='valborro@princeton.edu',
     version='0.1',
-    packages=['hsso'],
+    packages=find_packages(),
     requires=[
         "numpy",
         "matplotlib",
