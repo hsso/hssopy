38416610998a2d17a7653e08a46c3bdd5775c44a
Miguel de Val-Borro <miguel.deval@gmail.com>
2014-09-24 13:08:44 -0400

    Remove __name__ == '__main__' check

--- hsso/hipe.py
+++ hsso/hipe.py
@@ -25,36 +25,35 @@ parser.add_argument('-f', '--freq', default=None, type=float)
 parser.add_argument('--jpl', default="")
 args = parser.parse_args()
 
-if __name__ == "__main__":
-    for be in args.backend:
-        for p in args.pol:
-            hdulist = pyfits.open( glob.glob(
-                       join(args.datadir, args.obsid, 'level2',
-                       '{0}-{1}-{2}'.format(be, p, args.sideband),
-                       'box_001', '*.fits*'))[0])
-            for i in hdulist[1].header:
-                if i.find('META_') > 0 and hdulist[1].header[i] == 'loThrow':
-                    throw = hdulist[1].header[i[4:]]
-            print(throw)
-            for subband in args.subband:
-                if 'flux_{0}'.format(subband) in hdulist[1].data.names:
-                    freq = hdulist[1].data.field('{0}frequency_{1}'.format(
-                            args.sideband.lower(), subband))[0]
-                    flux = hdulist[1].data.field('flux_{0}'.format(subband))[0]
-                    plt.plot(freq, flux, drawstyle='steps-mid',
-                             label='{0}-{1} subband {2}'.format(be, p, subband))
-    if args.freq:
-        plt.axvline(x=args.freq, linestyle='--')
-        plt.axvline(x=args.freq-throw, linestyle=':')
-    if args.jpl:
-        # Read catalog data file
-        x0, x1 = plt.gca().get_xlim()
-        images = jpl.JPLmol(args.jpl).trans
-        img = images[(x0*1e3 < images['FREQ']) & (images['FREQ'] < x1*1e3) &
-                    (images['LGINT'] > -3.4)
-                    ]
-        for j in img:
-            print(j['FREQ']*1e-3, j['LGINT'])
-            plt.axvline(x=j['FREQ']*1e-3, linestyle='--')
-    plt.legend()
-    plt.show()
+for be in args.backend:
+    for p in args.pol:
+        hdulist = pyfits.open( glob.glob(
+                   join(args.datadir, args.obsid, 'level2',
+                   '{0}-{1}-{2}'.format(be, p, args.sideband),
+                   'box_001', '*.fits*'))[0])
+        for i in hdulist[1].header:
+            if i.find('META_') > 0 and hdulist[1].header[i] == 'loThrow':
+                throw = hdulist[1].header[i[4:]]
+        print(throw)
+        for subband in args.subband:
+            if 'flux_{0}'.format(subband) in hdulist[1].data.names:
+                freq = hdulist[1].data.field('{0}frequency_{1}'.format(
+                        args.sideband.lower(), subband))[0]
+                flux = hdulist[1].data.field('flux_{0}'.format(subband))[0]
+                plt.plot(freq, flux, drawstyle='steps-mid',
+                         label='{0}-{1} subband {2}'.format(be, p, subband))
+if args.freq:
+    plt.axvline(x=args.freq, linestyle='--')
+    plt.axvline(x=args.freq-throw, linestyle=':')
+if args.jpl:
+    # Read catalog data file
+    x0, x1 = plt.gca().get_xlim()
+    images = jpl.JPLmol(args.jpl).trans
+    img = images[(x0*1e3 < images['FREQ']) & (images['FREQ'] < x1*1e3) &
+                (images['LGINT'] > -3.4)
+                ]
+    for j in img:
+        print(j['FREQ']*1e-3, j['LGINT'])
+        plt.axvline(x=j['FREQ']*1e-3, linestyle='--')
+plt.legend()
+plt.show()
