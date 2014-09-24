43e85d336a6e928cc269e66144d31e8c4adf0eb2
Miguel de Val-Borro <miguel.deval@gmail.com>
2014-09-24 15:44:17 -0400

    Remove deprecated update keyword

--- wcslint.py
+++ wcslint.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python
 
-import pyfits, sys
+import sys
+from astropy.io import fits as pyfits
 
 hdus = pyfits.open(sys.argv[1], mode='update')
 
 for i in hdus:
 #     if 'CUNIT1' in i.header and 'Degrees' in i.header['CUNIT1']:
-    i.header.update('CUNIT1', 'deg')
-    i.header.update('CUNIT2', 'deg')
+    i.header['CUNIT1'] = 'deg'
+    i.header['CUNIT2'] = 'deg'
 hdus.flush()
+hdus.close()
