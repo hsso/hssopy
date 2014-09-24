1cf1963f909f4ed6fe8785cb81c82ee3ec4a1404
Miguel de Val-Borro <miguel.deval@gmail.com>
2014-09-24 17:33:01 -0400

    Check CUNIT1 is present in header

--- wcslint.py
+++ wcslint.py
@@ -6,8 +6,8 @@ from astropy.io import fits as pyfits
 hdus = pyfits.open(sys.argv[1], mode='update')
 
 for i in hdus:
-#     if 'CUNIT1' in i.header and 'Degrees' in i.header['CUNIT1']:
-    i.header['CUNIT1'] = 'deg'
-    i.header['CUNIT2'] = 'deg'
+    if 'CUNIT1' in i.header and 'Degrees' in i.header['CUNIT1']:
+        i.header['CUNIT1'] = 'deg'
+        i.header['CUNIT2'] = 'deg'
 hdus.flush()
 hdus.close()
