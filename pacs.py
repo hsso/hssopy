14c9893ee1d8b57c95a367b0faffcb83aa17bd74
Miguel de Val-Borro <miguel.deval@gmail.com>
2014-09-25 12:04:11 -0400

    Specify hdu in FITSFigure

--- pacs.py
+++ pacs.py
@@ -17,14 +17,16 @@ args = parser.parse_args()
 
 fitsfile = pacsfits(args.datadir, args.obsid, args.band[0])
 hdus = pyfits.open(fitsfile)
-gc = aplpy.FITSFigure(fitsfile)
+gc = aplpy.FITSFigure(fitsfile, hdu=1)
 ra = hdus[0].header['RA_NOM']
 dec = hdus[0].header['DEC_NOM']
 # Center at the comet position
-# gc.recenter(ra, dec, width=0.02,height=0.02)
-gc.show_markers(ra, dec, marker="+", color="white")
-gc.show_colorscale(pmin=0.6)
+gc.recenter(ra, dec, width=0.02,height=0.02)
+# gc.show_colorscale(pmin=20, vmid=-5e-5, stretch="log")
+gc.show_colorscale(vmin=1e-10)
 # gc.show_colorscale(stretch="log", vmid=-0.0001)
 # gc.show_colorscale(stretch="arcsinh")
 gc.show_contour(colors="white")
+gc.show_markers(ra, dec, marker="+", facecolor="white", edgecolor="white")
+gc.add_colorbar()
 gc.save('{}_{}.png'.format(args.obsid, args.band))
