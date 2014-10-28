6d5f7da188886007fda37de9a38a67eb3ffd54b3
Miguel de Val-Borro <miguel@archlinux.net>
2014-10-28 13:32:56 -0400

    Edit PACS quick view script

--- pacs.py
+++ pacs.py
@@ -7,6 +7,8 @@ from herschel import pacsfits
 from astropy.io import fits as pyfits
 import argparse
 import aplpy
+import numpy as np
+import matplotlib.pyplot as plt
 
 parser = argparse.ArgumentParser()
 parser.add_argument('obsid', default="")
@@ -17,13 +19,19 @@ args = parser.parse_args()
 
 fitsfile = pacsfits(args.datadir, args.obsid, args.band[0])
 hdus = pyfits.open(fitsfile)
+print(np.max(hdus[1].data))
+plt.imshow(hdus[1].data[310:350,180:240], origin="lower")
+plt.savefig('imshow.png')
+plt.show()
+plt.close()
+
 gc = aplpy.FITSFigure(fitsfile, hdu=1)
 ra = hdus[0].header['RA_NOM']
 dec = hdus[0].header['DEC_NOM']
 # Center at the comet position
 gc.recenter(ra, dec, width=0.02,height=0.02)
 # gc.show_colorscale(pmin=20, vmid=-5e-5, stretch="log")
-gc.show_colorscale(vmin=1e-10)
+gc.show_colorscale(vmin=1e-4)
 # gc.show_colorscale(stretch="log", vmid=-0.0001)
 # gc.show_colorscale(stretch="arcsinh")
 gc.show_contour(colors="white")
